35b00447878a1bfa3894d634b29bb0b5d0ea1224
Matheus Henrique <matheus.ferreira@ludospro.com.br>
2022-03-22 20:06:33 -0300

    First Commit

---
+++ SemantixTestApi/SemantixTestApi/Controllers/CurrencyController.cs
@@ -0,0 +1,69 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SemantixTestApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CurrencyController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<CurrencyController> _logger;
+
+        public CurrencyController(ILogger<CurrencyController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet(Name = "Dolar")]
+        public IEnumerable<WeatherForecast> Dolar()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet(Name = "Euro")]
+        public IEnumerable<WeatherForecast> Euro()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet(Name = "Libra")]
+        public IEnumerable<WeatherForecast> Libra()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ SemantixTestWeb/src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App-main">
+      <header className="App-header">dsaf </header>
+      <div className="App-sonteiner">
+        <div className="App-conteiner-header">S</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
